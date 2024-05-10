569741a7117114211d99e622dec501ff171ca9a4
Anshul <99967537+anshul-22@users.noreply.github.com>
2024-05-10 14:26:49 +0530

    Add files via upload

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TodoList(props) {
+  return (
+    <li className='list-item'>
+        {props.item}
+        <span class="icons">
+        <i class="fa-solid fa-trash icon-delete" onClick={
+            e=>{
+                props.deleteItem(props.index)
+            }
+        }></i>
+        </span>
+    </li>
+
+  )
+}
+
+export default TodoList
